48eaa2b7a95bbc14537bf13fcfd4daa37fe8d252
mariofandom <92304738+mariofandom@users.noreply.github.com>
2021-12-21 14:24:11 +0200

    Update kyve_rebuild.sh

--- kyve_rebuild.sh
+++ kyve_rebuild.sh
@@ -1,32 +1,81 @@
 #!/bin/bash
 
-docker pull kyve/evm:v0.0.11 && \
+docker pull kyve/evm:latest && \
 docker stop kyve-avalanche-node 2>/dev/null; \
 docker container rm kyve-avalanche-node 2>/dev/null; \
 docker run -d -it --restart=always \
---name kyve-avalanche-node kyve/evm:v0.0.11 \
---pool 0xd1EAe9CC4C0cC8D82c5800e2dAE972A70f2C4d0d \
---private-key key \
---stake stakeamount \
--e https://rpc.testnet.moonbeam.network
+--name kyve-avalanche-node kyve/evm:latest \
+--pool 0x464200b29738367366FDb4c45f3b8fb582AE0Bf8 \
+--private-key $key \
+--stake $stakeamount \
 
-docker pull kyve/evm:v0.0.11 && \
+docker pull kyve/evm:latest && \
 docker stop kyve-moonriver-node 2>/dev/null; \
 docker container rm kyve-moonriver-node 2>/dev/null; \
 docker run -d -it --restart=always \
---name kyve-moonriver-node kyve/evm:v0.0.11 \
---pool 0x5A679d476757C18Ec49dfB6c3c3511c8E8a76906 \
---private-key key \
---stake stakeamount \
--e https://rpc.testnet.moonbeam.network
+--name kyve-moonriver-node kyve/evm:latest \
+--pool 0x610D55fA573Bce4D2d36e8ADAAee517B785a69dF \
+--private-key $key \
+--stake $stakeamount \
 
-docker pull kyve/cosmos:v0.0.0 && \
+
+docker pull kyve/cosmos:latest && \
 docker stop kyve-cosmos-node 2>/dev/null; \
 docker container rm kyve-cosmos-node 2>/dev/null; \
 docker run -d -it --restart=always \
---name kyve-cosmos-node kyve/cosmos:v0.0.0 \
---pool 0x83748889798a93e4a816a6a9D2ecD40377D5530B \
---private-key key \
---stake stakeamount \
--e https://rpc.testnet.moonbeam.network
+--name kyve-cosmos-node kyve/cosmos:latest \
+--pool 0x7Bb18C81BBA6B8dE8C17B97d78B65327024F681f \
+--private-key $key \
+--stake $stakeamount \
+
+
+docker pull kyve/celo:latest && \
+docker stop kyve-celo-node 2>/dev/null; \
+docker container rm kyve-celo-node 2>/dev/null; \
+docker run -d -it --restart=always \
+--name kyve-celo-node kyve/celo:latest \
+--pool 0x1588fd93715Aa08d67c32C6dF96fC730B15E1E1A \
+--private-key $key \
+--stake $stakeamount \
+
+docker pull kyve/solana-snapshots:latest && \
+docker stop kyve-solana-node 2>/dev/null; \
+docker container rm kyve-solana-node 2>/dev/null; \
+docker run -d -it --restart=always \
+--name kyve-solana-node kyve/solana-snapshots:latest \
+--pool 0x3124375cA4de5FE5afD672EF2775c6bdcA1Cfdcc \
+--private-key $key \
+--stake $stakeamount \
+
+docker pull kyve/evm:latest && \
+docker stop kyve-evmos_evm-node 2>/dev/null; \
+docker container rm kyve-evmos_evm-node 2>/dev/null; \
+docker run -d -it --restart=always \
+--name kyve-evmos_evm-node kyve/evm:latest \
+--pool 0x24E7b48c3a6E40ea0e50764E617906c9B7cf9F21 \
+--private-key $key \
+--stake $stakeamount \
+
+docker pull kyve/cosmos:latest && \
+docker stop kyve-evmos_tendermint-node 2>/dev/null; \
+docker container rm kyve-evmos_tendermint-node 2>/dev/null; \
+docker run -d -it --restart=always \
+--name kyve-evmos_tendermint-node kyve/cosmos:latest \
+--pool 0xAa3337d1f8F72D544f3843B2089d2DA02BBcbD28 \
+--private-key $key \
+--stake $stakeamount \
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
